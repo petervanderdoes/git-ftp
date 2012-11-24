022c8620a8af60e99ff2c9ed94b7088538681011
Peter van der Does <github@avirtualhome.com>
2012-11-23 19:29:35 -0500

    Preparation for new development cycle after release 1.0.0

--- git-ftp.py
+++ git-ftp.py
@@ -141,7 +141,7 @@ def parse_args():
     if len(args) > 1:
         parser.error("too many arguments")
     if options.show_version:
-        version_str = "1.0.0"
+        version_str = "1.1.0-dev.1"
         print "git-ftp version %s " % (version_str)
         sys.exit(0)
     if args: cwd = args[0]
