5a9102323a90886ace6c8468af9aadd8d09f1180
Peter van der Does <github@avirtualhome.com>
2012-11-23 20:19:59 -0500

    Version bump 1.1.0-dev.4

--- git-ftp.py
+++ git-ftp.py
@@ -145,7 +145,7 @@ def parse_args():
     if len(args) > 1:
         parser.error("too many arguments")
     if options.show_version:
-        version_str = "1.1.0-dev.1"
+        version_str = "1.1.0-dev.4"
         print "git-ftp version %s " % (version_str)
         sys.exit(0)
     if args: cwd = args[0]
