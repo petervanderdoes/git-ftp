fab5e07883be1fe9917334c3e4b62fb8735fe429
Peter van der Does <peter@avirtualhome.com>
2016-08-06 19:27:55 -0400

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 setup(name='git-ftp',
-      version='1.3.0-dev.27',
+      version="1.3.0-dev.30"
       author='Peter van der Does',
       author_email='peter@avirtualhome.com',
       license='MIT',
