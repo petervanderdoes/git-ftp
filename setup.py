8e326341b1c54d7e23c0324c94e547f678779678
Peter van der Does <peter@avirtualhome.com>
2016-08-06 20:16:13 -0400

    Use setup for installation

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
-from setuptools import setup, find_packages
-with open('README.rst') as f:
-    readme = f.read()
+from setuptools import setup
+
 setup(name='git-ftp',
-      version="1.3.0-dev.30"
+      version="1.3.0-dev.30",
       author='Peter van der Does',
       author_email='peter@avirtualhome.com',
       license='MIT',
       description='Quick and efficient publishing of Git repositories over FTP',
-      long_description=readme,
-      packages=find_packages())
+      packages=['git-ftp'],
+      include_package_data=True,
+      package_data={'': ['README.rst']
+                    },
+      install_requires=['pathspec>=0.4.0',
+                        'GitPython>=2.0.8',
+                        ],
+      tests_require=['nose>=1.1.2',
+                     ],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Software Development',
+                   ]
+      )
